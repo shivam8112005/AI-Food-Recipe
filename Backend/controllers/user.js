c7741c91439b88c07210758f1f72ffc2f64d5c9f
shivam shukla <shivamshukla8112005@gmail.com>
2025-07-06 11:50:39 +0530

    Implemented signup route using JWT

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,5 +1,6 @@
 const User=require("../models/user");
-
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 const userSignUp=async(req,res)=>{
 const {email, password}=req.body;
 if(!email || !password){
@@ -9,6 +10,13 @@ let user = await User.findOne({email});
 if(user){
     return res.status(400).json({error:"Email is already exist !"})
 }
+const hashpass=await bcrypt.hash(password, 10);
+
+const userobj=new User({email, password:hashpass});
+ await userobj.save();
+ let token= jwt.sign({email, id:userobj._id}, process.env.SECRET_KEY);
+ return res.status(201).json({token,user});
+
 }
 
 const userLogin=async(req,res)=>{
