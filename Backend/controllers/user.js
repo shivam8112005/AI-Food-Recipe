82db73fd7544d21be091a50a43c411c8e16fd655
shivam shukla <shivamshukla8112005@gmail.com>
2025-07-06 12:22:36 +0530

    create login rojute with JWT auth

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -15,12 +15,22 @@ const hashpass=await bcrypt.hash(password, 10);
 const userobj=new User({email, password:hashpass});
  await userobj.save();
  let token= jwt.sign({email, id:userobj._id}, process.env.SECRET_KEY);
- return res.status(201).json({token,user});
+ return res.status(201).json({token,userobj});
 
 }
 
 const userLogin=async(req,res)=>{
-    
+    const {email, password}=req.body;
+    if(!email || !password){
+    return res.status(400).json({message:"emmail and password required"});
+}
+let user = await  User.findOne({email});
+if(user && await bcrypt.compare(password, user.password)){
+    let token= jwt.sign({email, id:user._id}, process.env.SECRET_KEY);
+ return res.status(200).json({token,user});
+}else{
+    return res.status(400).json({message:"Invalid Credentials!"});
+}
 }
 
 const getUser=async(req,res)=>{
